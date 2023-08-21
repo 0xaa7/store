8c7db1a0cea0b5077b768bf86a9f3185673f7058
Aamsher <hpandian008@gmail.com>
2023-08-21 18:17:08 +0530

    added header,Homepage and productlist components

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function Homepage() {
+  return (
+    <div>
+      <Typography variant="h4">Welcome to our Clothing Store!</Typography>
+      {/* Add more content here */}
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Grid,Card, CardMedia, CardContent, Typography } from '@mui/material'
+
+const ProductList = () => {
+  const products = [
+  {id:1, name:'product 1', price:'10$', imageUrl: '/images/dress_shirt_PNG8117.png'},
+  {id:1, name:'product 1', price:'10$', imageUrl: '/images/dress_shirt_PNG8117.png'},
+  {id:1, name:'product 1', price:'10$', imageUrl: '/images/dress_shirt_PNG8117.png'},
+  {id:1, name:'product 1', price:'10$', imageUrl: '/images/dress_shirt_PNG8117.png'}
+
+]
+
+  return (
+    <Grid container spacing={2} >
+      {products.map((product) => (
+        <Grid sx={{ width: '220px' }} item  key={product.id}>
+          <Card sx={{ width:'200px', cursor: "pointer",'&:hover': {
+                      transform: 'translateY(-2px)',
+                      boxShadow: '0px 2px 10px gray',
+                   }}}>
+            <CardMedia sx={{ objectFit: 'contain'}} component="img" height="200" image={product.imageUrl} alt={product.name} />
+            <CardContent>
+              <Typography variant='h6'>{product.name}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                Price: {product.price}
+              </Typography>
+
+            </CardContent>
+
+          </Card>
+
+        </Grid>
+      ) )}
+
+    </Grid>
+  )
+}
+
+export default ProductList
